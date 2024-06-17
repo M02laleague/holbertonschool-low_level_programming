930218cd25854d4dc4ca754847b04daef22e4611
M02laleague <9558@holbertonstudents.com>
2024-06-17 13:03:37 +0200

    exo_0

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,9 +1,11 @@
-"main.c"
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	 return (0);
-}"main.c"
-int main(void)
-{
-	 return (0);
+	    return (0);
 }
