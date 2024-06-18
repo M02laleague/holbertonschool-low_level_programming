977149b6e61dc2e417cf9378787a666bfbeffca1
M02laleague <9558@holbertonstudents.com>
2024-06-18 15:00:15 +0200

    exo_6

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Prints a Text
+ *
+ * Write a C program that prints
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * 6. Size is not grandeur, and territory does not make a nation
+ */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("Size of a char: %lu byte(s)\n", sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", sizeof(b));
-	printf("Size of a long int: %lu  byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", sizeof(e));
+	
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu  byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
