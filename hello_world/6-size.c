3848e934c2968810f5afe4f1dac1588afdf0b49b
M02laleague <9558@holbertonstudents.com>
2024-06-18 16:04:38 +0200

    exo_6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,6 +5,7 @@
  * Description: print the message the size of various
  * Return: value 0
  */
+
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -12,5 +13,6 @@ int main(void)
 	printf("Size of a long int: %lu  byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 	return (0);
 }
