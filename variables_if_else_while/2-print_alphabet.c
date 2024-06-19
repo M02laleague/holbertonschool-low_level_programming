335eccc06cc34a6bcd3c9d7b45362d21c1214014
M02laleague <9558@holbertonstudents.com>
2024-06-19 11:51:51 +0200

    Task_2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints the alphabet in lowercase
+ * description:Prints a random number
+ * Return: value 0
+ */
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
