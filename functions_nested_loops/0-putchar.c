0d6b4032ffd80c509bdb2802dafb8b7129691728
M02laleague <9558@holbertonstudents.com>
2024-06-20 16:30:41 +0200

    Task_0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: value 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
