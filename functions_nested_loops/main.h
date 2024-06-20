b1f6e1da24e7f0d21dd9e7e97edcf998590b49f6
M02laleague <9558@holbertonstudents.com>
2024-06-20 16:43:52 +0200

    Task_1

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-//bibliothèque regoupant les fontions prototype du projet 3
+/*bibliothèque regoupant les fontions prototype du projet 3*/
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
