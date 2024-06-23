26e0ff8a84cac0a0e74513374d41581324bed5a9
M02laleague <9558@holbertonstudents.com>
2024-06-23 23:17:25 +0200

    Task_5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 /**
 * print_sign - print if is 0
 * @n: is the parameter to use in this function
@@ -7,19 +7,19 @@
 
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n == 0)
 	{
-	_putchar ('+');
-	return (1);
+		_putchar ('0');
+		return (0);
 	}
-	else if (n == 0)
+	else if (n > 0)
 	{
-	_putchar ('0');
-	return (0);
+		_putchar ('+');
+		return (1);
 	}
-	else if (n < 0)
+	else
 	{
-	_putchar ('-');
-	return (-1);
+		_putchar ('-');
+		return (-1);
 	}
 }
