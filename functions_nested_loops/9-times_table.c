33c6050429e283debfc22a8ffaa4a305dfb7fadd
M02laleague <9558@holbertonstudents.com>
2024-06-23 23:05:20 +0200

    Task_9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,12 +1,32 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * times_table - print multiplication tables from 0 to 9.
  */
-int main(void)
+void times_table(void)
 {
-    times_table();
-    return (0);
+	int tables, column;
+
+	for (tables = 0; tables <= 9; tables++)
+	{
+		_putchar('0');
+		for (column = 1; column <= 9; column++)
+		{
+			if ((tables * column) <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((tables * column) + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(((tables * column) / 10) + '0');
+				_putchar(((tables * column) % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
