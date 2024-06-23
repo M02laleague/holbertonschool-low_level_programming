f1bcad69d2e819a6cd3e5c88fe18e7969517cac6
M02laleague <9558@holbertonstudents.com>
2024-06-23 23:07:55 +0200

    Task_10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,9 +1,13 @@
-include "main.h"
+#include "main.h"
 
-/**
- * Write a program that prints _putchar, followed by a new line..
- *
- * Return: value 0.
- */
-int main(void)
+/** 
+* add - print the result of the sum
+* @a: the firts parameter
+* @b: the second number
+* Return: Always 0.
+*/
+
+int add(int a, int b)
 {
+	return (a + b);
+}
