c77f9657bc834cbf41ece07e0a3241ac356311f4
M02laleague <9558@holbertonstudents.com>
2024-06-24 12:28:41 +0200

    Task_3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,14 @@
  * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
  */
+
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i + '0');
+	_putchar(i + '0');
 	}
 
 	_putchar('\n');
