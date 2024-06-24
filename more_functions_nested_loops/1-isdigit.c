ff06ec0b1209ad7362d699668fb296354af7100e
M02laleague <9558@holbertonstudents.com>
2024-06-24 10:39:18 +0200

    Task_1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isdigit - This function verify if c is digit
+* @c: the parameter char to comparate.
+* Return: 1 or 0.
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
