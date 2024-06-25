410768e69ceec732cbe1154744c2a6c6539c5e9d
M02laleague <9558@holbertonstudents.com>
2024-06-25 10:12:38 +0200

    Task_10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,7 +9,7 @@ void print_triangle(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
-	} 
+	}
 	else
 	{
 	int i, j;
@@ -20,12 +20,10 @@ void print_triangle(int size)
 	{
 	_putchar(' ');
 	}
-
 	for (j = 1; j <= i; j++)
 	{
 	_putchar('#');
 	}
-
 	_putchar('\n');
 	}
 	}
