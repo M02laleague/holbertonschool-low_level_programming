832c1f29569f01d3fc5f4571b1f1ac1c9296f408
M02laleague <9558@holbertonstudents.com>
2024-06-25 12:25:51 +0200

    Go

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,14 @@ int main(void)
 
 	for (i = 2; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf(" FizzBuzz");
-		else if (i % 3 == 0)
-			printf(" Fizz");
-		else if (i % 5 == 0)
-			printf(" Buzz");
-		else
-			printf(" %d", i);
+	if (i % 3 == 0 && i % 5 == 0)
+		printf(" FizzBuzz");
+	else if (i % 3 == 0)
+		printf(" Fizz");
+	else if (i % 5 == 0)
+		printf(" Buzz");
+	else
+		printf(" %d", i);
 	}
 	printf("\n");
 
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+death in commmmmmmmmmmmmmmming 
