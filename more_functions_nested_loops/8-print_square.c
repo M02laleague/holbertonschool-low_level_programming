ffcebd2f17dcdfde0d13a90cf9cdae2ace71f02d
M02laleague <9558@holbertonstudents.com>
2024-06-25 10:35:43 +0200

    Task_7

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -6,21 +6,15 @@
  */
 void print_square(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
 	int i, j;
 
-	for (i = 0; i < size; i++)
-	{
-	for (j = 0; i < size; j++)
-	{
-	_putchar('#');
-	}
-	_putchar('\n');
-	}
-	}
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
 }
---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
