1b1ebd6d4e5f48ce99a78b0efc9e9bcf36646295
M02laleague <9558@holbertonstudents.com>
2024-06-27 15:36:53 +0200

    Task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,5 +7,16 @@
 void print_rev(char *s)
 {
 	int i, j, len;
-	int 0
-	
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+	i++;
+	}
+	len = i;
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
