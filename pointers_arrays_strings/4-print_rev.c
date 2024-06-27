3c74cd2e30cb8ed65776dc1ab5c1d57707a766e7
M02laleague <9558@holbertonstudents.com>
2024-06-27 09:44:23 +0200

    Delete Main.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i, j, len;
+	int 0
+	
